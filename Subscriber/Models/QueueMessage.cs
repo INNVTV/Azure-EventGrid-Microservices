dfe74f33a0f94769cf1b27b0a9255610cd93524c
Kaz <kaz@innvtv.com>
2018-10-19 12:16:31 -0700

    Added EventCount to QueueMessage

--- Subscriber/Models/QueueMessage.cs
+++ Subscriber/Models/QueueMessage.cs
@@ -5,5 +5,6 @@ namespace Subscriber.Models
         public string Topic { get; set; }
         public string Source { get; set; }
         public string EventType { get; set; }
+        public int EventCount {get; set;}
     }
 }
